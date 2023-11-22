4edf87009eb7d0a916284aa6890f65b1352552f9
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2023-11-22 10:09:49 -0500

    Update 5_views.py

--- django/5_views.py
+++ django/5_views.py
@@ -10,13 +10,33 @@ class <name_of_class>(TemplateView):
 ```
 
 
-# Vistas (views) --Logica/controlador
-# This view is for list
-# **PruebaListView**
-```py
-class ListarPruebaView(ListView):
-    queryset # -- Podemos imprimir una lista
-    model = Prueba #Aqui va el modelo Model
-    template_name = 'home/prueba.html'
-    context_object_name = 'lista_prueba' #Variable que accede al contexto de la lista en el template seria {{lista_prueba}}
-```
+class ListAllEmpleados(ListView):
+    """Listar todos los empleados de una empresa"""
+    template_name = 'empleado/list_empleados.html'
+    model = Empleado
+    context_object_name = 'lista_empleados'
+
+class ListByAreaEmpleados(ListView):
+    """Listar empleados de la empresa de un area"""
+    template_name = 'empleado/list_empleado_area.html'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        short_name = self.kwargs['short_name']        # Por medio del PATH
+        queryset = Empleado.objects.filter(
+            departamento__short_name = short_name
+        )
+
+        return queryset
+
+class ListEmpleadosByKword(ListView):
+    """ List of employers with a word"""
+
+    template_name = 'empleado/by_kword.html'
+    context_object_name = 'lista_empleados'
+
+    def get_queryset(self) -> QuerySet[Any]:        # Por medio de un kword/form
+        kword_kword = self.request.GET.get('kword', '')
+        queryset = Empleado.objects.filter(
+            first_name = kword_kword
+        )
+        return queryset
