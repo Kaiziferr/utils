b6ebd6db798673d85b106c785d3bcd78a20960cb
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2024-01-08 20:33:14 -0500

    Update admin_configuration.py

--- django/admin/admin_configuration.py
+++ django/admin/admin_configuration.py
@@ -9,22 +9,13 @@ class EmpleadoAdmin(admin.ModelAdmin):
         'first_name',
         'last_name',
         'department',
-        'job'
+        'job',
+        'full_name' # Valor compuesto
     ) # lista los atributos que se quiere listar en el display
 
-from django.contrib import admin
-from .models import Empleado, Habilidades
-# Register your models here.
-
-
-class EmpleadoAdmin(admin.ModelAdmin):
-
-    list_display = (
-        'first_name',
-        'last_name',
-        'department',
-        'job'
-    ) # lista los atributos que se quiere listar en el display
+    def full_name(self, obj): # Debe tener el mismo nombre
+        fname = f'{obj.first_name} {obj.last_name}'
+        return fname
 
     search_fields = ('first_name', ) # Buscador
     list_filter = ('job', 'skills') # Ventana comporta como un filtro
