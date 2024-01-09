dba41733b49c6c45529bb42cd00c6074aaa85c61
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2024-01-08 20:59:09 -0500

    Update listviews.py

--- django/views/listviews.py
+++ django/views/listviews.py
@@ -4,3 +4,19 @@ class PruebaListView(ListView):
   queryset = ['A', 'B', 'C', 'D'] # queryset database
   context_object_name = 'lista_ejemplo'
 ```
+
+
+```py
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Empleado
+class ListAllEmpleados(ListView):
+    template_name = 'empleado/listar_empleados.html'
+    paginate_by = 4
+    model = Empleado
+    context_object_name = 'listar_empleado'
+```
+
+# Paginacion
+# http://localhost:8000/listar-empleados/?page=2
