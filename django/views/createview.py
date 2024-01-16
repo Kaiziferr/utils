27ea5615c70cbfb6b3f6925b64ef8bf729d3f354
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2024-01-15 22:59:51 -0500

    Create createview.py

---
+++ django/views/createview.py
@@ -0,0 +1,8 @@
+
+class RegistroEmpleado(CreateView):
+    model = Empleado
+    template_name = 'empleado/register_empleado.html'
+    #fields = ['first_name', 'last_name']
+    fields = ('__all__')
+    success_url = '.' # recharge the same page
+    
