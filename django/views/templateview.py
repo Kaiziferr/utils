2deb6664cf64a6aa8adfa5e3bc2f32a95e5747d0
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2024-01-23 09:49:37 -0500

    Update templateview.py

--- django/views/templateview.py
+++ django/views/templateview.py
@@ -1,1 +1,10 @@
+class SuccessView(TemplateView):
+    template_name = 'empleado/success.html'
 
+class RegistroEmpleado(CreateView):
+    model = Empleado
+    template_name = 'empleado/register_empleado.html'
+    #fields = ['first_name', 'last_name']
+    fields = ('__all__')
+    success_url = reverse_lazy('success/') # recharge the same page
+# La idea es que redireccione, cuando se ejecute el proceso.
