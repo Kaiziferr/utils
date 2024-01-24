3999eea85c099457c372197c32bf625e8e1df9f4
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2024-01-24 10:09:12 -0500

    Update delete_views.py

--- django/views/delete_views.py
+++ django/views/delete_views.py
@@ -3,3 +3,11 @@ class EmpleadoDeleteView(DeleteView):
     model = Empleado
     template_name = 'empleado/delete_empleado.html'
     success_url = reverse_lazy('empleado_app:correcto')
+
+# Requiere un form
+<h1>ELiminar empleado </h1>
+
+<form method="POST"> {% csrf_token %}
+    <p>Confirmar</p>
+    <button type="submit">Confirmar</button>
+</form>
