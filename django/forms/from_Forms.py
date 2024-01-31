cde070160415d06432be34fae3b2030438767b61
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2024-01-31 16:04:42 -0500

    Create from_Forms.py

---
+++ django/forms/from_Forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Departamento
+
+class NewDepartamentForm(forms.Form):
+
+    nombre = forms.CharField(max_length=50)
+    apellidos = forms.CharField(max_length=50)
+    departamento = forms.CharField(max_length=50)
+    short_name = forms.CharField(max_length=20)
