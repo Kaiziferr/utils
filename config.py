f3e0de22ab6bb7c8fafdcafffe14570ea89117ca
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2024-09-30 16:31:34 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 sns.set(style="darkgrid")
+sns.set(font_scale=1)
 pd.set_option('display.float_format', '{:,.2f}'.format)
 title_data = 'Materiales extraidos en Colombia'
 paleta = sns.color_palette('Set2').as_hex()
